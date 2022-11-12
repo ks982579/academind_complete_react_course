ea331c084f845eb13456950e34c8caaef9ed948b
ks982579 <kevinsullivan@ksulldev.space>
2022-11-12 15:48:54 +0000

    finished lecture 153

--- section_12/01-starting-setup/src/App.js
+++ section_12/01-starting-setup/src/App.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './App.css';
+import Button from './components/UI/Button/Button';
 
 function App() {
-  return (
-    <div className="app">
-      <h1>Hi there!</h1>
-    </div>
-  );
+    const [showPara, setShowPara] = useState(false);
+
+    const toggleParagraphHandler = (evnt) => {
+        setShowPara((prevState) => {
+            return !prevState;
+        })
+    };
+
+    return (
+        <div className="app">
+            <h1>Hi there!</h1>
+            { showPara && <p>This is new!</p>}
+            <Button onClick={toggleParagraphHandler}>Toggle Paragraph!</Button>
+        </div>
+    );
 }
 
 export default App;
