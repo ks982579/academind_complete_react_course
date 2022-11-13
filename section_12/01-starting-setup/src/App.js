acf093672fe25fd7f4df54c05a52efc38dd67d5d
ks982579 <kevinsullivan@ksulldev.space>
2022-11-13 18:31:16 +0000

    Lesson 157: more useCallback and JS closures

--- section_12/01-starting-setup/src/App.js
+++ section_12/01-starting-setup/src/App.js
@@ -6,17 +6,25 @@ import Button from './components/UI/Button/Button';
 
 function App() {
     const [showPara, setShowPara] = useState(false);
+    const [allowToggle, setAllowToggle] = useState(false)
 
     const toggleParagraphHandler = useCallback((evnt) => {
-        setShowPara((prevState) => {
-            return !prevState;
-        });
-    },[]);
+        if(allowToggle){
+            setShowPara((prevState) => {
+                return !prevState;
+            });
+        }
+    },[allowToggle]);
+
+    const allowToggleHandler = () => {
+        setAllowToggle(true);
+    };
 
     return (
         <div className="app">
             <h1>Hi there!</h1>
             <DemoOutput show={showPara}/>
+            <Button onClick={allowToggleHandler}>Allow Toggle</Button>
             <Button onClick={toggleParagraphHandler}>Toggle Paragraph!</Button>
         </div>
     );
