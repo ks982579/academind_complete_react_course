e96410bb0a1f6637cdbef645d42e2dbccd16af87
ks982579 <kevinsullivan@ksulldev.space>
2022-11-27 16:34:43 +0000

    lesson 178: Handling Loading

--- section_14/01-starting-project/src/App.js
+++ section_14/01-starting-project/src/App.js
@@ -4,9 +4,11 @@ import MoviesList from './components/MoviesList';
 import './App.css';
 
 function App() {
-  const [movies, setMovies] = useState([])
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
   
   async function fetchMoviesHandler() {
+    setIsLoading(true);
     const response = await fetch("https://swapi.dev/api/films/")
     const data = await response.json();
     const transformedMovies = data.results.map(movieData => {
@@ -17,6 +19,7 @@ function App() {
         releaseDate: movieData.release_date,
       }
     });
+    setIsLoading(false);
     setMovies(transformedMovies);
   };
 
@@ -26,7 +29,9 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length === 0 && <p>Found no Movies</p>}
+        {isLoading && <p>Loading...</p>}
       </section>
     </React.Fragment>
   );
