2d15653272a1fe62ee724bf1ba5083259d9f1cc3
ks982579 <kevinsullivan@ksulldev.space>
2022-12-10 15:23:36 +0000

    Lesson 189: adding configuration to custom hooks

--- section_15/01-starting-project/src/hooks/use-counter.js
+++ section_15/01-starting-project/src/hooks/use-counter.js
@@ -1,15 +1,19 @@
 import React, {useState, useEffect} from 'react';
 
-const useCounter = () => {
+const useCounter = (countForwards = true) => {
     const [counter, setCounter] = useState(0);
 
     useEffect(() => {
         const interval = setInterval(() => {
-            setCounter((prevState) => prevState + 1);
+            if (countForwards) {
+                setCounter((prevState) => prevState + 1);
+            } else {
+                setCounter((prevState) => prevState - 1);
+            }
         }, 1000);
 
         return () => clearInterval(interval);
-    }, []);
+    }, [countForwards]);
 
     return counter;
 };
