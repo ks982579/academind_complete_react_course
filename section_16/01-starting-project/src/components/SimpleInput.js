90d5624e2a1aa09b238571f710b2a0ece9b40b77
ks982579 <kevinsullivan@ksulldev.space>
2022-12-16 07:50:47 +0000

    lesson 203: adding validation onBlur

--- section_16/01-starting-project/src/components/SimpleInput.js
+++ section_16/01-starting-project/src/components/SimpleInput.js
@@ -10,6 +10,15 @@ const SimpleInput = (props) => {
     setEnteredName(event.target.value);
   }
 
+  const nameInputBlurHandler = event => {
+    setEnteredNameTouched(true);
+
+    if (enteredName.trim() === '') {
+      setEnteredNameIsValid(false);
+      return;
+    }
+  }
+
   const formSubmissionHandler = event => {
     event.preventDefault();
 
@@ -34,7 +43,7 @@ const SimpleInput = (props) => {
     <form onSubmit={formSubmissionHandler}>
       <div className={nameInputClasses}>
         <label htmlFor='name'>Your Name</label>
-        <input ref={nameInputRef} type='text' id='name' onChange={nameInputChangeHandler}/>
+        <input ref={nameInputRef} type='text' id='name' onChange={nameInputChangeHandler} onBlur={nameInputBlurHandler}/>
         {nameInputIsInvalid && <p className='error-text'>Name must not be empty.</p>}
       </div>
       <div className="form-actions">
