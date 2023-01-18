4ee6210a4c8baf98090b7445d65c8c3b392c076a
ks982579 <kevinsullivan@ksulldev.space>
2023-01-18 08:30:04 +0000

    Lesson 231: adding actions to reducer

--- section_18/01-redux/redux-demo.js
+++ section_18/01-redux/redux-demo.js
@@ -1,9 +1,17 @@
 const redux = require('redux');
 
 //reducer function
-const counterReducer = (state = { counter: 0 }, action) => {
-    return {
-        counter: state.counter + 1
+const counterReducer = (state = { counter: 0 }, { type }) => {
+    if (type === 'increment') {
+        return {
+            counter: state.counter + 1
+        };
+    } else if (type === 'decrement') {
+        return {
+            counter: state.counter - 1
+        };
+    } else {
+        return { counter: state.counter };
     };
 };
 
@@ -20,3 +28,4 @@ const counterSubscriber = () => {
 store.subscribe(counterSubscriber);
 
 store.dispatch({ type: 'increment' });
+store.dispatch({ type: 'decrement' });
